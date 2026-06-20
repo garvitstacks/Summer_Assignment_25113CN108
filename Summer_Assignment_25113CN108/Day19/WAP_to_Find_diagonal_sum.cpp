e54c9198e8d92f7f273492f83c2c5bc87671c83f
garvitstacks <garvitsingh6900@gmail.com>
2026-06-20 12:27:27 +0530

    Add remaining work

---
+++ Summer_Assignment_25113CN108/Day19/WAP_to_Find_diagonal_sum.cpp
@@ -0,0 +1,25 @@
+                        //Write a program to Find diagonal sum.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter order of square matrix: ";
+    cin >> n;
+
+    int a[n][n];
+    cout << "Enter matrix:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j]; }}
+
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += a[i][i]; }
+
+    cout << "Diagonal Sum = " << sum;
+
+    return 0;
+}
